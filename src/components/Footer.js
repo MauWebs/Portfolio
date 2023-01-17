bca04688e11b385d1c5601581fd73cb7d3f6622a
MauWebs <mauriciovidalwebs@gmail.com>
2023-01-17 14:11:14 -0300

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,30 +9,30 @@ function Footer() {
                 <div class="footer__container">
                     <div class="footer__top">
                         <div class="company__info">
-                            <a href="/"><h2 class="company__logo">Mau Webs</h2></a>
+                            <a href="/" rel="noreferrer"><h2 class="company__logo">Mau Webs</h2></a>
                             <div>
                                 <ul class="components">
                                     <div></div>
                                     <li>
-                                        <a href="https://m.facebook.com/profile.php?id=100086829729023" target="_blank">
+                                        <a href="https://m.facebook.com/profile.php?id=100086829729023" rel="noreferrer" target="_blank">
 
                                             <span class="fa fa-facebook"></span>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://twitter.com/Mau_Webs" target="_blank">
+                                        <a href="https://twitter.com/Mau_Webs" rel="noreferrer" target="_blank">
 
                                             <span class="fa fa-twitter"></span>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.instagram.com/mau_webs/" target="_blank">
+                                        <a href="https://www.instagram.com/mau_webs/" rel="noreferrer" target="_blank">
 
                                             <span class="fa fa-instagram"></span>
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="https://www.linkedin.com/in/mauricio-vidal-webs" target="_blank">
+                                        <a href="https://www.linkedin.com/in/mauricio-vidal-webs" rel="noreferrer" target="_blank">
 
                                             <span class="fa fa-linkedin"></span>
                                         </a>
